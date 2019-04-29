006b7e9180380c1113fd89f57566f781506624ac
Marcel <marcelkaemper862@gmail.com>
2019-04-29 21:51:26 +0200

    Added mousecallback function

--- mouseInteraction.cpp
+++ mouseInteraction.cpp
@@ -1,11 +1,17 @@
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 using namespace cv;
+
+void mouseCallback(int event, int x, int y, int flags, void* userdata);
 
 int main(int argc, char** argv){
 	
 	Mat image = imread("../data/lena.jpg", 1);
 
+	setMouseCallback("Image", mouseCallback, NULL);
+
+	std::cout << "test";
 
 
 	imshow("Image", image);
@@ -15,4 +21,10 @@ int main(int argc, char** argv){
 	}
 	
 	return 0;
+}
+
+void mouseCallback(int event, int x, int y, int flags, void* userdata){
+	if(event == EVENT_LBUTTONDOWN){
+			std::exit(0);
+	}	
 }
