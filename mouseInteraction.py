be1968d2c2f348dc4f4e3fb9cdea036cf8e7e229
Marcel <marcelkaemper862@gmail.com>
2019-04-29 19:37:34 +0200

    Added mouseInteraction Source

--- mouseInteraction.py
+++ mouseInteraction.py
@@ -1,8 +1,14 @@
 import cv2
 
+def test(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("LBUTTON")
+
 img1 = cv2.imread("./data/lena.jpg", -1)
 
+cv2.namedWindow("image", flags=cv2.WINDOW_GUI_NORMAL)
 cv2.imshow("image", img1)
+cv2.setMouseCallback("image", test)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
