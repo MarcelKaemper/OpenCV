710d37abcbb25f27a5af611b49bc6332d6bd688e
Marcel <marcelkaemper862@gmail.com>
2019-04-30 16:52:41 +0200

    Changed size

--- shapes.cpp
+++ shapes.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv){
 
 	rectangle(image, Point(0,0), Point(255,255), Scalar(255,0,0), FILLED);
 	line(image, Point(255,255), Point(500,500), Scalar(255,0,0), 20);
-	ellipse(image, RotatedRect(Point2f(200,200), Size2f(100,100) , 100), Scalar(0,255,0));
+	ellipse(image, RotatedRect(Point2f(200,200), Size2f(100,100) , 10), Scalar(0,255,0));
 
 	imshow("Image", image);
 
