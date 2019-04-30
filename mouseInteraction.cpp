eea5d540d1a5071aea735d19d0166c6feb008bd1
Marcel <marcelkaemper862@gmail.com>
2019-04-30 16:52:33 +0200

    Added feedback on mouseclick

--- mouseInteraction.cpp
+++ mouseInteraction.cpp
@@ -9,22 +9,20 @@ int main(int argc, char** argv){
 	
 	Mat image = imread("../data/lena.jpg", 1);
 
+	imshow("Image", image);
 	setMouseCallback("Image", mouseCallback, NULL);
 
-	std::cout << "test";
-
-
-	imshow("Image", image);
 
 	while(1){
 		waitKey(0);
 	}
 	
+	destroyAllWindows();
 	return 0;
 }
 
 void mouseCallback(int event, int x, int y, int flags, void* userdata){
 	if(event == EVENT_LBUTTONDOWN){
-			std::exit(0);
+			std::cout << "Click" << std::endl;
 	}	
 }
